c21e219c420147d7d743e0455ea8df0c4872ca41
Richa Sharma <richa@weboniselab.com>
2012-03-07 15:02:07 +0530

    files_deleted

--- array_of_words_using_recursive_fun.php
+++
@@ -1,6 +0,0 @@
-<?php
-$given_string = "PHP parses a file by looking for <br/> one of the special tags that tells it to start interpreting the text as PHP code. The parser then executes all of the code it finds until it runs into a PHP closing <br/> tag.";
-$array_of_words = str_word_count($given_string,1);
-print_r("The array of the words available in the given string are : ");
-print_r($array_of_words);
-?>
--- find_position_of_php_in_string1.php
+++
@@ -1,6 +0,0 @@
-<?php
-$given_string = "PHP parses a file by looking for <br/> one of the special tags that tells it to start interpreting the text as PHP code. The parser then executes all of the code it finds until it runs into a PHP closing <br/> tag.";
-$string_to_search = "PHP";
-$first_occurence = strpos($given_string, $string_to_search);
-print "The first occurence of the string '$string_to_search' is in the '$first_occurence' position...";
-?>
